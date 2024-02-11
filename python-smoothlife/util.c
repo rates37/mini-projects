acd2f865e463ce9341669262c498acf14be6ba0e
Satya Jhaveri <satyajhaveri@protonmail.ch>
2024-02-11 11:58:36 +1100

    Corrected C implementation

--- python-smoothlife/util.c
+++ python-smoothlife/util.c
@@ -1,47 +1,47 @@
 #include <math.h>
 //  gcc -fPIC -shared -lm -o util.so util.c
-float update_grid(int width, int height, float *grid, float *nextGrid, int r_a);
-float sigma_1(float x, float a, float alpha);
-float sigma_2(float x, float a, float b);
-float sigma_m(float x, float y, float m);
-float s(float n, float m);
-float alpha_m = 0.147;
-float alpha_n = 0.028;
-float b_1 = 0.278;
-float b_2 = 0.365;
-float d_1 = 0.267;
-float d_2 = 0.445;
+void update_grid(int width, int height, double *grid, double *nextGrid, int r_a);
+double sigma_1(double x, double a, double alpha);
+double sigma_2(double x, double a, double b);
+double sigma_m(double x, double y, double m);
+double s(double n, double m);
+double alpha_m = 0.147;
+double alpha_n = 0.028;
+double b_1 = 0.278;
+double b_2 = 0.365;
+double d_1 = 0.267;
+double d_2 = 0.445;
 
-float sigma_1(float x, float a, float alpha){
+double sigma_1(double x, double a, double alpha){
     return 1 / (1 + exp(-(x-a)*4/alpha));
 }
 
 
-float sigma_2(float x, float a, float b){
+double sigma_2(double x, double a, double b){
     return sigma_1(x,a, alpha_n) * (1 - sigma_1(x, b, alpha_n));
 }
 
 
-float sigma_m(float x, float y, float m){
+double sigma_m(double x, double y, double m){
     return x * (1 - sigma_1(m, 0.5, alpha_m)) + y * sigma_1(m, 0.5, alpha_m);
 }
 
 
-float s(float n, float m){
+double s(double n, double m){
     return sigma_2(n, sigma_m(b_1, d_1, m), sigma_m(b_2, d_2, m));
 }
 
-float update_grid(int width, int height, float *grid, float *nextGrid, int r_a) {
+void update_grid(int width, int height, double *grid, double *nextGrid, int r_a) {
     for (int xi = 0; xi < width; xi++) {
         for (int yi = 0; yi < height; yi++) {
-            float m = 0;
-            float M = 0;
-            float n = 0;
-            float N = 0;
-            float r_i = r_a / 3;
+            double m = 0;
+            double M = 0;
+            double n = 0;
+            double N = 0;
+            double r_i = r_a / 3;
 
             for (int dx = -(r_a-1); dx < r_a; dx++) {
-                for (int dy = -(r_a-1); dy < r_a; dx++) {
+                for (int dy = -(r_a-1); dy < r_a; dy++) {
                     int x = (((xi+dx)%width + width)%width);
                     int y = (((yi+dy)%height + height)%height);
                     if (dx*dx + dy*dy <= r_i * r_i) {
@@ -53,15 +53,15 @@ float update_grid(int width, int height, float *grid, float *nextGrid, int r_a) 
                     }
                 }
             }
-            nextGrid[yi * width + xi] = 2*s(m/M, n/N) - 1;
+            nextGrid[yi*width + xi] = 2*s(m/M, n/N) - 1;
         }
     }
 
     for (int xi = 0; xi < width; xi++) {
         for (int yi = 0; yi < height; yi++) {
-            grid[yi * width + xi] += nextGrid[yi * width + xi];
-            if (grid[yi * width + xi] > 1) grid[yi * width + xi] = 1;
-            if (grid[yi * width + xi] < 0) grid[yi * width + xi] = 0;
+            grid[yi*width + xi] += 0.05*nextGrid[yi*width + xi];
+            if (grid[yi*width + xi] < 0) grid[yi*width + xi] = 0;
+            if (grid[yi*width + xi] > 1) grid[yi*width + xi] = 1;
         }
     }
 }
