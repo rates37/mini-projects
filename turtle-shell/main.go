9313612cc6a16de0c329f3323cb9d259959d271c
rates37 <satyajhaveri@protonmail.ch>
2026-06-12 00:53:30 +1000

    Handle exit command

--- turtle-shell/main.go
+++ turtle-shell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,6 +21,33 @@ func main() {
 			}
 			fmt.Println("Error reading input:", err)
 			continue
+		}
+
+		// split arguments:
+		input = strings.TrimSpace(input)
+		if input == "" { // ignore empty input
+			continue
+		}
+		args := strings.Split(input, " ")
+
+		// check for exit command or empty input:
+		if args[0] == "exit" {
+			if len(args) > 1 {
+				// return exit code if provided:
+				fmt.Printf("Exiting with code %s\n", args[1])
+				// check args[1] is numeric:
+				var exitCode int
+				if _, err := fmt.Sscanf(args[1], "%d", &exitCode); err != nil {
+					fmt.Println("Invalid exit code, exiting with code 0")
+					os.Exit(0)
+				} else {
+					// convert to int and exit with code:
+					fmt.Sscanf(args[1], "%d", &exitCode)
+					os.Exit(exitCode)
+				}
+			}
+			fmt.Println("Goodbye!")
+			break
 		}
 
 		fmt.Fprintln(os.Stdout, "🐢 💬:", input)
